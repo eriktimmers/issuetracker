57feb7d63d15fa9a393dc3e22023ee968bceba7e
Erik Timmers <erik.timmers@kega.nl>
2024-04-26 17:16:20 +0200

    [MAIN] Add an issue Api Post request

---
+++ issue-tracker/app/api/issues/route.tsx
@@ -0,0 +1,29 @@
+import {NextRequest, NextResponse} from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+
+const newIssueSchema = z.object({
+    title: z.string().min(1).max(255),
+    description: z.string().min(1)
+});
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+
+    const validation = newIssueSchema.safeParse(body);
+
+    if (!validation.success)
+        return NextResponse.json({error: validation.error.errors}, { status: 400 })
+
+    const newUser = await prisma.issue.create({
+        data: {
+            title: body.title,
+            description: body.description
+        }
+    })
+
+    return NextResponse.json(
+        newUser,
+        { status: 201 }
+    )
+}
