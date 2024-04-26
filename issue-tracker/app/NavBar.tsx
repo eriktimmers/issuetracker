e08d3495c8ffa84e886e3250c542fabc78222c04
Erik Timmers <erik.timmers@kega.nl>
2024-04-26 15:26:32 +0200

    [MAIN] Highlighting the current page

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+'use client';
 import Link from "next/link";
 import { GiLongAntennaeBug } from "react-icons/gi";
-import {yellow} from "next/dist/lib/picocolors";
+import {usePathname} from "next/navigation";
+import classNames from "classnames";
 
 const NavBar = () => {
+    const currentPage = usePathname();
+
     const links = [
         {name: 'Admin', href: '/'},
         {name: 'Issues', href: '/issues'},
     ];
+
     return (
         <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
             <Link href="/"><GiLongAntennaeBug className='text-amber-200 size-8'/></Link>
             <ul className="flex space-x-6">
                 {links.map((link, index) => (
                     <li key={index}>
-                        <Link className='text-zinc-200 hover:text-zinc-400 transition-colors' href={link.href}>{link.name}</Link>
+                        <Link className={classNames(
+                            {'text-zinc-0': currentPage === link.href,
+                                  'text-zinc-300': currentPage !== link.href },
+                            'hover:text-zinc-400 transition-colors'
+                        )} href={link.href}>{link.name}</Link>
                     </li>
                 ))}
             </ul>
---
+++ issue-tracker/app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            Issues page
+        </div>
+    );
+};
+
+export default Page;
