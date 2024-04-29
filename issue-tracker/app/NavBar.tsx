6614a28c599db84246df05746fbfdd8b3841677f
Erik Timmers <erik.timmers@kega.nl>
2024-04-29 10:02:16 +0200

    [MAIN] Set up Radix

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -14,13 +14,13 @@ const NavBar = () => {
 
     return (
         <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-            <Link href="/"><GiLongAntennaeBug className='text-amber-200 size-8'/></Link>
+            <Link href="/"><GiLongAntennaeBug className='text-amber-800 size-8'/></Link>
             <ul className="flex space-x-6">
                 {links.map((link, index) => (
                     <li key={index}>
                         <Link className={classNames(
-                            {'text-zinc-0': currentPage === link.href,
-                                  'text-zinc-300': currentPage !== link.href },
+                            {'text-zinc-900': currentPage === link.href,
+                                  'text-zinc-500': currentPage !== link.href },
                             'hover:text-zinc-400 transition-colors'
                         )} href={link.href}>{link.name}</Link>
                     </li>
--- issue-tracker/app/issues/page.tsx
+++ issue-tracker/app/issues/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Button} from "@radix-ui/themes";
 
 const Page = () => {
     return (
         <div>
             Issues page
+            <Button>New Issue</Button>
         </div>
     );
 };
